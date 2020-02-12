b9f4f0733b3599c0b3387ae051efecf7b7e90652
mohd danish <mddanishyusuf@gmail.com>
2020-02-13 00:11:54 +0530

    minor changes again

--- src/Application.js
+++ src/Application.js
@@ -11,7 +11,7 @@ function Application() {
     const [myMonitors, setMyMonitors] = useState([])
 
     useEffect(() => {
-        axios.get(process.env.UPTIME_ROBOT_NOCODEAPI).then(result => {
+        axios.get(process.env.UPR_NOCODEAPI).then(result => {
             setMyMonitors(result.data.monitors)
         })
     },[])
